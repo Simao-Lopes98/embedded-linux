8d51fd7a2342e696f96e105fc277a79dca4f2460
Simao Lopes <simaolopes98@gmail.com>
2024-12-29 22:57:35 +0000

    Added config.in . Updated blink code to legacy support

--- blink.c
+++ blink.c
@@ -1,33 +1,89 @@
+// blink.c
+
+#include <gpiod.h>
+#include <error.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <wiringPi.h>
+#include <string.h>
+#include <unistd.h>
 
+#define GPIO_CHIP "/dev/gpiochip0"
+#define GPIO_CONSUMER "blink_routine"
 
+void gpio_cleanup(void);
 
-#define LED_PIN 0 // GPIO 27 on RasPi
+struct gpiod_chip *chip;
+struct gpiod_line_request_config config;
+struct gpiod_line_bulk lines;
 
+int main(int argc, char *argv[]) 
+{
+    unsigned int offsets[1];
+    int values[1];
+    int err;
+    int counter = 10;
 
-int main() {
-    if(wiringPiSetup() == -1)
-    {
-        printf ("Failed to init wiringPi\n");
-        return 1;
-    }
-
-    pinMode (LED_PIN, OUTPUT);
-
-    printf ("Starting to blink\n");
-
-
-    while (1)
-    {
-        digitalWrite(LED_PIN, HIGH);
-        printf("LED is ON\n");
-        delay(500);
-        digitalWrite(LED_PIN, LOW);
-        printf("LED is off\n");
-        delay(500);
-    }
+    chip = gpiod_chip_open (GPIO_CONSUMER);
     
+    if (chip == NULL)
+    {
+        perror ("gpiod_chip_open");
+        gpio_cleanup();
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    // Set pin 3 (GPIO 2) to HIGH
+    offsets[0] = 3;
+    values[0] = 0;
+
+    err = gpiod_chip_get_lines (chip, offsets, 1, &lines);
+
+    if (err != 0)
+    {
+        perror ("gpiod_chip_get_lines");
+        gpio_cleanup();
+        return EXIT_FAILURE;
+    }
+
+    //config config
+    memset(&config, 0, sizeof(config));
+    config.consumer = GPIO_CONSUMER;
+    config.request_type = GPIOD_LINE_REQUEST_DIRECTION_OUTPUT;
+
+    //get the bulk lines setting default value to 0. Takes ownership of liones
+    err = gpiod_line_request_bulk (&lines, &config, values);
+    if (err != 0)
+    {
+        perror ("gpiod_line_request_bulk");
+        gpio_cleanup();
+        return EXIT_FAILURE;
+    }
+
+    values[0] = 1;
+    while (counter != 0)
+    {
+        //output value 
+        err = gpiod_line_set_value_bulk (&lines, values);
+        if (err != 0)
+        {
+            perror ("gpiod_line_set_value_bulk");
+            gpio_cleanup();
+            return EXIT_FAILURE; 
+        }
+
+        sleep(1); // Wait 1 sec
+        counter--;
+        values[0] = !values[0];
+    }
+
+    gpio_cleanup();
+
+    return EXIT_SUCCESS;
+}
+
+
+void gpio_cleanup(void)
+{
+    gpiod_line_release_bulk (&lines);
+    gpiod_chip_close (chip);
 }
---
+++ my_blink_app.mk
@@ -0,0 +1,14 @@
+MY_BLINK_APP_VERSION = 1.0
+MY_BLINK_APP_SITE = $(TOPDIR)/../my_blink_app
+MY_BLINK_APP_SITE_METHOD = local
+MY_BLINK_APP_DEPENDENCIES = wiringpi
+
+define MY_BLINK_APP_BUILD_CMDs
+	$(MAKE) -C $(@D)
+endef
+
+define MY_BLINK_APP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/blink $(TARGET_DIR)/usr/bin/blink
+endef
+
+$(eval $(generic-package))
